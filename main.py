8e234b4f8feff4af35d3c8bf678f4b43f2eb09c7
Oleksii Anoshyn <42578692+Greeengooo@users.noreply.github.com>
2021-08-17 13:45:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    s = 'Hello World!'
+    return {
+        'statusCode': 200,
+        'body': json.dumps(s)
+    }
